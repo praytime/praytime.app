0c5a4bbbe54431b541c23d21435750520693a962
theimpostor <sahirhoda@gmail.com>
2018-09-09 20:28:34 +0000

    Change sw cache to "network falling back to cache"

--- public/sw.js
+++ public/sw.js
@@ -25,8 +25,9 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request, {ignoreSearch: true}).then(response => {
-      return response || fetch(event.request)
-    })
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request)
+      })
   )
 })
