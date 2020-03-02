8a5b8df1735c61cfd139a8997a5e20fdf1a03cb9
theimpostor <sahirhoda@gmail.com>
2020-03-02 08:14:45 -0600

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const distDir = path.resolve(__dirname, 'dist')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: '[name].[contenthash].js',
+    path: distDir
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // hash: true,
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+      template: './src/index.html'
+    })
+  ]
 }
