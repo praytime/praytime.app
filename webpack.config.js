fa00f8c70ad6f155bcf2a41e3d55ab851c16d571
theimpostor <sahirhoda@gmail.com>
2021-12-18 09:12:40 -0600

    use separate webpack entries for service workers in prep for firebase modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const path = require('path')
@@ -7,21 +6,34 @@ const srcDir = path.resolve(__dirname, 'src')
 const staticDir = path.resolve(srcDir, 'static')
 const distDir = path.resolve(__dirname, 'dist')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[contenthash].js',
-    path: distDir
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: staticDir, to: distDir }
-      ]
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
-}
+module.exports = [
+  {
+    entry: './src/index.js',
+    output: {
+      filename: '[name].[contenthash].js',
+      path: distDir
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: staticDir, to: distDir }
+        ]
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
+    ]
+  }, {
+    entry: './src/sw.js',
+    output: {
+      filename: 'sw.js',
+      path: distDir
+    }
+  }, {
+    entry: './src/firebase-messaging-sw.js',
+    output: {
+      filename: 'firebase-messaging-sw.js',
+      path: distDir
+    }
+  }
+]
